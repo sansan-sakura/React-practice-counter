d4010bc4f9ef01d2044f3402109e9d4246c5a84a
Sakura Tanaka <sito6496t@google.com>
2023-09-26 11:30:13 +0200

    final version

--- src/App.js
+++ src/App.js
@@ -26,19 +26,32 @@ export default function App() {
     return anotherDay;
   };
 
-  const incrementStep = () => {
-    setStep((s) => s + 1);
-  };
+  // const incrementStep = () => {
+  //   setStep((s) => s + 1);
+  // };
 
-  const decrementStep = () => {
-    setStep((s) => s - 1);
-  };
+  // const decrementStep = () => {
+  //   setStep((s) => s - 1);
+  // };
 
   function addDays(days) {
     let date = new Date();
     const result = new Date(date);
     result.setDate(result.getDate() + days);
     return result;
+  }
+
+  function handleInputCount(value){
+   const num= Number(value)
+   const typeOfValue=typeof num
+ if(typeOfValue==="number" ) setCount(num)
+ console.log(value)
+
+  }
+
+  function handleReset(){
+    setCount(0)
+    setStep(1)
   }
 
   // function subtractDays(days) {
@@ -59,19 +72,27 @@ export default function App() {
 
   return (
     <div className="App">
+    
+  
       <div className="step_box">
-        <button onClick={decrementStep}>-</button>
+      <input type="range" min="1" max="10"value={step} onChange={(e) => {
+        setStep(Number(e.target.value))}}/>
+      <span>{step}</span>
+        {/* <button onClick={decrementStep}>-</button>
         <p>
-          Steps:<span> {step}</span>
+          Steps:<span> {step}</span> 
         </p>
-        <button onClick={incrementStep}>+</button>
+        <button onClick={incrementStep}>+</button>*/}
       </div>
       <div className="count_box">
-        <button onClick={decrementCount}>-</button>
+      <button onClick={decrementCount}>-</button>
+       <input type="text" onChange={(e)=>handleInputCount(e.target.value)} />
+        <button onClick={incrementCount}>+</button>
+        {/* <button onClick={decrementCount}>-</button>
         <p>
           Count:<span> {count}</span>
         </p>
-        <button onClick={incrementCount}>+</button>
+        <button onClick={incrementCount}>+</button> */}
       </div>
 
       <div>
@@ -87,6 +108,8 @@ export default function App() {
           {count !== 0 && count !== 1 && count !== -1 && "s"} from now is{" "}
           <span>{anotherDay.toLocaleDateString()}</span>
         </p>
+        {count !==0 || step!==1?   <button onClick={handleReset}>Reset</button>:null}
+      
       </div>
     </div>
   );
